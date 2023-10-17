3e9a93831dc55c1df2de16c7717cef233742b53f
Usman Zia <u_zia@uncg.edu>
2023-10-17 14:42:38 -0400

    added backend

---
+++ java/csc340/src/test/java/com/assignment/demo/Csc340ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Csc340ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
