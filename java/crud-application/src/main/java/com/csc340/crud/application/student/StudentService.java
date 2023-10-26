8b19bda63fcef149b3e9bd2e57166f00c381fa69
Usman Zia <u_zia@uncg.edu>
2023-10-26 15:27:33 -0400

    Final changes

--- java/crud-application/src/main/java/com/csc340/crud/application/student/StudentService.java
+++ java/crud-application/src/main/java/com/csc340/crud/application/student/StudentService.java
@@ -10,18 +10,35 @@ public class StudentService {
 	@Autowired
 	private StudentRepository studentRepository;
 	
+	/**
+	 * Get all Students
+	 * @return a List of Students
+	 */
 	public List<Student> getStudents() {
 		return studentRepository.findAll();
 	}
 	
+	/**
+	 * Get a Student by their Student ID
+	 * @param id, Student ID
+	 * @return Student that associates with the given Student ID
+	 */
 	public Student getStudent(int id) {
 		return studentRepository.getReferenceById(id);
 	}
 	
+	/**
+	 * Save a Student entry
+	 * @param newStudent, a Student
+	 */
 	public void saveStudent(Student newStudent) {
 		studentRepository.save(newStudent);
 	}
 	
+	/**
+	 * Delete a Student entry
+	 * @param id, Student ID
+	 */
 	public void deleteStudent(int id) {
 		studentRepository.deleteById(id);
 	}
